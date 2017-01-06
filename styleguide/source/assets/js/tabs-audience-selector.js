d0ef8e20d68146827b9d87730a3b42c0387abd95
Megh Plunkett <plunkett@palantir.net>
2017-01-06 12:32:42 -0500

    AMA-121: adds mobile styling for audience selector.

---
+++ styleguide/source/assets/js/tabs-audience-selector.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // Audience Selector tabs
+    $('.tabs-audience_selector_list').click(function() {
+        // Add the active class to the list when it is clicked.
+        $(this).toggleClass('open');
+    });
+});
