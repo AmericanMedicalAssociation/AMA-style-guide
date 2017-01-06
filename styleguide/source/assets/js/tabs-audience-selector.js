3c01b8482486cc6a95dd2e0a1d975854afc1389d
Megh Plunkett <plunkett@palantir.net>
2017-01-06 17:15:09 -0500

    AMA-121: adds interaction and states.

--- styleguide/source/assets/js/tabs-audience-selector.js
+++ styleguide/source/assets/js/tabs-audience-selector.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
-    // Audience Selector tabs
-    $('.tabs-audience_selector_list').click(function() {
-        // Add the active class to the list when it is clicked.
-        $(this).toggleClass('open');
-    });
+  // Mobile Audience Selector tabs
+  $('.tabs-audience_selector_list').click(function () {
+
+    // Add the active class to the list when it is clicked.
+    $(this).toggleClass('open');
+  });
+
 });
