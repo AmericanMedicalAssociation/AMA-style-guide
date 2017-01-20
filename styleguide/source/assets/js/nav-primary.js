5cc35926f56edf5daaa98f9decaae124e533133d
Megh Plunkett <plunkett@palantir.net>
2017-01-20 17:10:11 -0500

    AMA-102: adds subnavigation elements, styling and interaction.

--- styleguide/source/assets/js/nav-primary.js
+++ styleguide/source/assets/js/nav-primary.js
@@ -9,7 +9,9 @@ $(document).ready(function() {
   // Mobile Primary navigation functionality.
   $('.nav-primary_list').each(function () {
 
-    // Toggle the list open and closed when the menu button is clicked.
+    $('.nav-primary_list-item').addClass('nav-primary_list-item-hidden');
+
+    // Toggle the primary navigation open and closed when the Menu button is clicked.
     $('.nav-primary_button-menu').click(function () {
       $(this).toggleClass('nav-primary_button-menu-clicked');
       $('.nav-primary_list').toggleClass('nav-primary_list-closed nav-primary_list-open');
@@ -17,6 +19,17 @@ $(document).ready(function() {
       $('.nav-primary-menu_overlay-mobile').toggleClass('nav-primary-menu_overlay-mobile-on');
     });
 
-  });
+    // Primary nav list item click to open sub-navigation.
+    $('.nav-primary_list-item_title a').click(function () {
+      var parent = $(this).parent();
 
+      // Hide the sub-navigation menus initially.
+      $('.nav-primary_list_subnav').removeClass('nav-primary_list_subnav-initial');
+
+      // Show the sub-navigation of the item that is clicked.
+      parent.addClass('nav-primary_list-item-active').removeClass('nav-primary_list-item-hidden');
+      parent.children('.nav-primary_list-item_title').addClass('nav-primary_list-item_title-active');
+    });
+
+  });
 });
