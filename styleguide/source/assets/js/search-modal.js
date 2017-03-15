8449aa41547c47be39466e05fa597f18faa52cde
Carl Martens <carlrmartens@gmail.com>
2017-03-15 11:25:15 -0500

    Added focus input state when search modal is open

--- styleguide/source/assets/js/search-modal.js
+++ styleguide/source/assets/js/search-modal.js
@@ -9,6 +9,7 @@ jQuery.noConflict();
 
   // Trigger events on search button click
   $('.button-search').click(function() {
+
     // Remove classes for nav dropdowns
     $('.nav-primary_list-item, .nav-primary_list-item_title').removeClass('is-active');
     $('.nav-primary_list_subnav').removeClass('is-open');
@@ -23,6 +24,11 @@ jQuery.noConflict();
     else {
       $('.search_modal').addClass('is-open');
       $('.nav-primary-menu_overlay-mobile').addClass('nav-primary-menu_overlay-mobile-on');
+
+      // Focus search input
+      setTimeout(function(){
+        $('.search_modal .search-field_input').focus();
+      }, 300);
     };
 
     // Remove nav primary list open class and add closed class on search button click
