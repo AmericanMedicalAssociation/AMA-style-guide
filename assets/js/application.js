25ca729045005904c743e79bc9cd15427ba59dc5
Avi Schwab <schwab@palantir.net>
2017-05-05 10:55:05 -0500

    Update 2017-05-05T15:55:01.998Z

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,5 +1,21 @@
 jQuery.noConflict();
 (function($) {
+  // Search
+
+  // When a user clicks on the ribbon trigger (main)
+  $('.accordion-trigger').click(function() {
+    // Unfocus on the trigger
+    $(this).blur();
+    // add a class to the sibling accordion content
+    $(this).toggleClass('is-active');
+    $(this).siblings('.accordion-content').toggleClass('is-active').slideToggle(300);
+  });
+
+
+})(jQuery);
+
+jQuery.noConflict();
+(function($) {
 
   // Hide items except first 3
   $('.audience_featured-hero_item').each(function() {
