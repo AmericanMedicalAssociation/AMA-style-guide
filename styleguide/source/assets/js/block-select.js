0adaba383f9401d60aa9cc5d82b7ac214069db68
Erica Garcia <garcialuck08@gmail.com>
2017-05-17 17:19:32 -0500

    Updates class names

--- styleguide/source/assets/js/block-select.js
+++ styleguide/source/assets/js/block-select.js
@@ -1,9 +1,9 @@
 jQuery.noConflict();
 (function($) {
-  $('.block-select--message').hide();
+  $('.block-select__message').hide();
 
   $('.block-select').click(function() {
     $(this).toggleClass('block-select--selected');
-    $('.block-select--message').toggle();
+    $('.block-select__message').toggle();
   });
 })(jQuery);
