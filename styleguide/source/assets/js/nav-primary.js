566fb580a0c15fecc38e233ec0bb1dbc89d5d5e4
Avi Schwab <schwab@palantir.net>
2017-05-18 14:13:45 -0500

    Stop action from triggering when the menu isn’t open.

--- styleguide/source/assets/js/nav-primary.js
+++ styleguide/source/assets/js/nav-primary.js
@@ -117,13 +117,16 @@ jQuery.noConflict();
 
   // Close the menu and sub menu when user taps outside of subnav.
   $('.body, header:not(ul.nav-primary_list)').on('touchstart click', function (event) {
-    if (!$(event.target).closest('#nav-primary, .nav-primary-menu_button').length || $(event.target).hasClass('nav-primary_list-item') || $(event.target).hasClass('nav-primary_list_subnav')) {
-      $('.nav-primary_list').removeClass('nav-primary_list-open').addClass('nav-primary_list-closed');
-      $('.nav-primary-menu_overlay-mobile').removeClass('nav-primary-menu_overlay-mobile-on');
-      $('.nav-primary-menu_button').removeClass('nav-primary-menu_button-clicked');
-      $('.nav-primary_list_subnav').removeClass('is-open');
-      $('.nav-primary_list-item_title').removeClass('is-active');
-      $('.nav-primary_list-item').removeClass('is-active');
+    if($('.nav-primary_list_subnav.is-open').length){
+      if (!$(event.target).closest('#nav-primary, .nav-primary-menu_button').length || $(event.target).hasClass('nav-primary_list-item') || $(event.target).hasClass('nav-primary_list_subnav')) {
+        console.log("hi");
+        $('.nav-primary_list').removeClass('nav-primary_list-open').addClass('nav-primary_list-closed');
+        $('.nav-primary-menu_overlay-mobile').removeClass('nav-primary-menu_overlay-mobile-on');
+        $('.nav-primary-menu_button').removeClass('nav-primary-menu_button-clicked');
+        $('.nav-primary_list_subnav').removeClass('is-open');
+        $('.nav-primary_list-item_title').removeClass('is-active');
+        $('.nav-primary_list-item').removeClass('is-active');
+      }
     }
   });
 
