143ec2016e1676e8afa0ad1dd8c003234d2cdb6d
Avi Schwab <schwab@palantir.net>
2017-07-28 16:11:10 -0500

    Update documentation for using JS template.

---
+++ styleguide/docs/JS_TEMPLATE.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * {{ A brief description of what this Javascript does. }}
+ *
+ * JavaScript should be made compatible with libraries other than jQuery by
+ * wrapping it with an "anonymous closure". See:
+ * - https://drupal.org/node/1446420
+ * - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+ */
+(function ($, Drupal) {
+  Drupal.behaviors.{{ some unique name }} = {
+      attach: function (context, settings) {
+          (function ($) {
+              {{ script here }}
+          })(jQuery);
+      }
+  };
+})(jQuery, Drupal);
