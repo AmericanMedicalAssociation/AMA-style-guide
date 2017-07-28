d566719966af36af472f1426c47d8b5c801b7dfd
Rachel Ponce <rachel.ponce@ama-assn.org>
2017-07-28 13:52:35 -0500

    Update 2017-07-28T18:52:32.924Z

--- assets/js/hub-cta-half-video.js
+++ assets/js/hub-cta-half-video.js
@@ -1,22 +1,6 @@
-/**
- * @file
- * Initialize fitVid for YouTube vieos.
- *
- * JavaScript should be made compatible with libraries other than jQuery by
- * wrapping it with an "anonymous closure". See:
- * - https://drupal.org/node/1446420
- * - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
- */
- (function ($, Drupal) {
-   Drupal.behaviors.fitvidinit = {
-     attach: function (context, settings) {
-       (function ($) {
-
-         $(document).ready(function(){
-           $('iframe[src*="youtube"]').parent().fitVids();
-         });
-
-       })(jQuery);
-     }
-   };
- })(jQuery, Drupal);
+jQuery.noConflict();
+(function($) {
+  $(document).ready(function(){
+    $('iframe[src*="youtube"]').parent().fitVids();
+  });
+})(jQuery);
