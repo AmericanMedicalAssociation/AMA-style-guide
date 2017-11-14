d5810b4eb77ccb98c4ba1753dc241add9da87f6c
Neil Gallagher <neil.gallagher@ama-assn.org>
2017-11-14 15:41:34 -0600

    EWL-4243 - backstop.js installation

---
+++ styleguide/tests/backstop/backstop_data/html_report/config.js
@@ -0,0 +1,29 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_homepage_0_sg-vp-wrap_0_phone.png",
+        "test": "../bitmaps_test/20171114-153907/backstop_default_homepage_0_sg-vp-wrap_0_phone.png",
+        "selector": "#sg-vp-wrap",
+        "fileName": "backstop_default_homepage_0_sg-vp-wrap_0_phone.png",
+        "label": "homepage",
+        "misMatchThreshold": 0.1,
+        "error": "Reference file not found /Users/ngallagh/projects/AMA-style-guide/styleguide/tests/backstop/backstop_data/bitmaps_reference/backstop_default_homepage_0_sg-vp-wrap_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_homepage_0_sg-vp-wrap_1_tablet.png",
+        "test": "../bitmaps_test/20171114-153907/backstop_default_homepage_0_sg-vp-wrap_1_tablet.png",
+        "selector": "#sg-vp-wrap",
+        "fileName": "backstop_default_homepage_0_sg-vp-wrap_1_tablet.png",
+        "label": "homepage",
+        "misMatchThreshold": 0.1,
+        "error": "Reference file not found /Users/ngallagh/projects/AMA-style-guide/styleguide/tests/backstop/backstop_data/bitmaps_reference/backstop_default_homepage_0_sg-vp-wrap_1_tablet.png"
+      },
+      "status": "fail"
+    }
+  ]
+});
